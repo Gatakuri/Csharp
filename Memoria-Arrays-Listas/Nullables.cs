34063b43ea81aed6ffa0862efa5cf0b70a479c4c
Gatakuri <138379688+Gatakuri@users.noreply.github.com>
2024-09-01 19:34:33 -0300

    Create Nullables.cs

---
+++ Memoria-Arrays-Listas/Nullables.cs
@@ -0,0 +1,49 @@
+using System;
+
+
+
+namespace Nullables {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            //Nullables podem ser escritos assim:
+            Nullable<int> v1 = null;
+            //Ou assim
+            int? v2 = null;
+            //Para indicar que os structs podem ser nulos
+
+
+            //Métodos com nullables:
+
+            //GetValueOrDefault - Pega o valor existente, em caso de nulo, o valor padrão
+            int? x = null;
+            int? y = 10;
+            Console.WriteLine(x.GetValueOrDefault());// 0
+            Console.WriteLine(y.GetValueOrDefault());// 10
+
+            //HasValue - Retorna True se tiver valor e False se for nulo
+            Console.WriteLine(x.HasValue);//False
+            Console.WriteLine(y.HasValue);//True
+
+            //Value - Retorna o valor de algo (se for nulo, quebra o programa)
+            //Para não quebrar, implementei uma lógica que ajuda a fazer um método seguro
+
+            if (x.HasValue) {
+                Console.WriteLine(x.Value);
+            }
+            else {
+                Console.WriteLine("X is null!");
+            }
+
+            if (y.HasValue) {
+                Console.WriteLine(y.Value);
+            }
+            else {
+                Console.WriteLine("Y is null!");
+            }
+        }
+
+    }
+}
