f300d85a2c7a61a231e7ff42fe92f62b118e8bc4
Gatakuri <138379688+Gatakuri@users.noreply.github.com>
2024-09-12 10:42:19 -0300

    Create Vetores-pt2-ProductClass.cs

---
+++ Memoria-Arrays-Listas/Vetores/Vetores-pt2-ProductClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores2
+{
+    internal class Product
+    {
+
+        public double Price { get; set; }
+        public string Name { get; set; }
+
+    }
+}
