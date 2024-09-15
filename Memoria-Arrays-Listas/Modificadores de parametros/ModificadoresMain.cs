75214c33a1e7de8b125d36f8bbaf060b019c39f9
Gatakuri <138379688+Gatakuri@users.noreply.github.com>
2024-09-15 19:00:41 -0300

    Update ModificadoresMain.cs

--- Memoria-Arrays-Listas/Modificadores de parametros/ModificadoresMain.cs
+++ Memoria-Arrays-Listas/Modificadores de parametros/ModificadoresMain.cs
@@ -1,30 +1,22 @@
+using Modificares_de_parametros;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Modificares_de_parametros {
-    internal class Calculator {
 
-        public static int sum(params int[] numbers) {
+namespace Modificadores {
+    class Program {
+        static void Main(string[] args) {
 
-            int sum = 0;
+            int result = Calculator.sum(1, 2, 3, 4 );
+            Console.WriteLine(result);
 
-            for (int i = 0; i < numbers.Length; i++) {
-                sum += numbers[i];
-            }
+            int a = 10;
+            Calculator.triple(ref a);
+            Console.WriteLine(a);
 
-            return sum;
+            int b = 10;
+            int _double;
+            Calculator.duplicate(b, out _double);
+            Console.WriteLine(_double);
         }
-
-        public static void triple(ref int x) {
-            x *= 3;
-        }
-
-        public static void duplicate(int origin, out int result) {
-            result = origin * 2;
-        }
-
     }
 }
