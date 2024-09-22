c1d205f3cdb1cecff431d2077e0feb8b6e0e36dc
Gatakuri <138379688+Gatakuri@users.noreply.github.com>
2024-09-22 15:27:36 -0300

    Create Main.cs

---
+++ Memoria-Arrays-Listas/Listas/Listas-exercicio/Main.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace Exercicio_listas {
+    class Program {
+        static void Main(string[] args) {
+            
+
+            Console.Write("How many employees will be registered?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+            Console.WriteLine();
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Employee #" + i);
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have the salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId);
+
+            if (emp != null) {
+
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine());
+                emp.IncreaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id not exist!");
+            }
+
+            Console.WriteLine("Updated list of employees:");
+
+            foreach (Employee obj in list) {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
